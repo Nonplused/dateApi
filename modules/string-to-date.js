ae9a16569e598abcad4b69b7f10acce0dec398a4
Mathias Graae <Mathias Graae>
2016-02-01 13:29:58 +0100

    Fixed bug in return

--- modules/string-to-date.js
+++ modules/string-to-date.js
@@ -5,5 +5,6 @@ module.exports = function (dateStr) {
     dateStr = +dateStr*1000;
   }
   var date = moment(dateStr);
+  if (!date.isValid()) return {'unix': null, 'natural': null};
   return {'unix': date.format('X'), 'natural': date.format('Do MMMM YYYY')};
 }

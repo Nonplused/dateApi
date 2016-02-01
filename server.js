afcbc9ca53ba82afe72bbbbd8fdac3ee6371a0e3
Mathias Graae <Mathias Graae>
2016-02-01 14:50:56 +0100

    added homepage

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ var express = require('express');
 
 //Express
 var app = express();
+app.set('views', './views');
 app.set('view engine', 'jade');
   
 //Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
 app.use('/api', require('./routes/api'));
 
 //Start server

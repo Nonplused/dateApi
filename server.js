d1d46cbbaa89ad97a7e2c8c1e706a5f8ad1c5944
Mathias Graae <Mathias Graae>
2016-02-01 16:10:31 +0100

    bug fix

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var express = require('express');
 
 //Express
 var app = express();
+var port = process.env.PORT || 5000;
 app.set('view engine', 'jade');
 app.set('views', './views');
-//app.set('port', (process.env.PORT || 5000));
+
 
 //Routes
 app.get('/', (req, res) => {
@@ -14,4 +15,6 @@ app.get('/', (req, res) => {
 app.use('/api', require('./routes/api'));
 
 //Start server
-app.listen(5000);
+app.listen(port, () => {
+  console.log('App running on port: ' + port);
+});
